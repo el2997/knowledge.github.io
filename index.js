c8a2fc9605fcad5c78adca600eb5cb152f986b0a
el2997 <eliomarlopez97@gmail.com>
2020-05-12 01:07:16 -0400

    update

--- index.js
+++ index.js
@@ -1,121 +1,107 @@
+HTML CSS JSResult
+EDIT ON
+//////////////settings/////////
+var actionZ = 0; //on left click action
+var rotationA = 3.1; // amount of rotation
+var movementSpeed = 10;
+var zoomSpeed = 10;
+var totalObjects = 40000;
+/////////////////////////////////
+var rotated = false;
+var container = document.createElement('div');
+document.body.appendChild( container );
 
-var SEPARATION = 40, AMOUNTX = 130, AMOUNTY = 35;
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1, 10000)
+camera.position.z = 2000;
 
-var container;
-var camera, scene, renderer;
-/*
+var scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2( 0x555555, 0.0003 );
+var geometry = new THREE.Geometry();
 
-if (window.WebGLRenderingContext){
-    renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    }
-else {
-    renderer = new THREE.CanvasRenderer();
-    }
-*/
-
-var particles, particle, count = 0;
-
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-
-init();
-animate();
-
-function init() {
-
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
-    if(container) {
-        container.className += container.className ? ' waves' : 'waves';
-    }
-
-    camera = new THREE.PerspectiveCamera( 120, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.y = 150; //changes how far back you can see i.e the particles towards horizon
-    camera.position.z = 300; //This is how close or far the particles are seen
-
-    camera.rotation.x = 0.35;
-
-    scene = new THREE.Scene();
-
-    particles = new Array();
-
-    var PI2 = Math.PI * 2;
-    var material = new THREE.SpriteCanvasMaterial( {
-
-        color: 0x939393, //changes color of particles
-        program: function ( context ) {
-
-            context.beginPath();
-            context.arc( 0, 0, 0.1, 0, PI2, true );
-            context.fill();
-
-        }
-
-    } );
-
-    var i = 0;
-
-    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-
-        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-
-            particle = particles[ i ++ ] = new THREE.Sprite( material );
-            particle.position.x = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 );
-            particle.position.z = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) - 10 );
-            scene.add( particle );
-
-        }
-
-    }
-
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor( 0xffffff, 1);
-    container.appendChild( renderer.domElement );
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
+for (i = 0; i < totalObjects; i ++)
+{
+  var vertex = new THREE.Vector3();
+  vertex.x = Math.random()*40000-20000;
+  vertex.y = Math.random()*7000-3500;
+  vertex.z = Math.random()*7000-3500;
+  geometry.vertices.push( vertex );
 }
+
+var material = new THREE.ParticleBasicMaterial( { size: 6 });
+var particles = new THREE.ParticleSystem( geometry, material );
+
+scene.add( particles );
+
+camera.position.x = -10000;
+
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild( renderer.domElement );
+
+renderer.render( scene, camera );
+
+render();
+
+            function render() {
+        requestAnimationFrame( render );
+        if (!rotated && camera.position.x < 11000)
+        {
+          if(camera.position.x > 10000){
+            rotated = true;
+            if (camera.rotation.y < rotationA){
+              camera.rotation.y += .015;
+              rotated = false;
+            }
+            if (camera.position.z > -2000){
+              camera.position.z -= 19;
+              rotated = false;
+            }
+          }
+          else{
+          camera.position.x += movementSpeed;
+          camera.position.z += actionZ;
+          }
+        }
+        else if(rotated && camera.position.x > -11000){
+                    if(camera.position.x < -10000){
+            rotated = false;
+            if (camera.rotation.y > 0){
+              camera.rotation.y -= .015;
+              rotated = true;
+            }
+            if (camera.position.z < 2000){
+              camera.position.z += 19;
+              rotated = true;
+            }
+          }
+          else{
+          camera.position.x -= movementSpeed;
+          camera.position.z -= actionZ;
+          }
+        }
+
+
+                renderer.render( scene, camera );
+
+            }
+
+window.addEventListener( 'mousedown', onDocumentMouseDown, false );
+window.addEventListener( 'mouseup', onDocumentMouseUp, false );
+window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize() {
+                camera.aspect = window.innerWidth / window.innerHeight;
+                camera.updateProjectionMatrix();
 
-    windowHalfX = window.innerWidth / 2;
-    windowHalfY = window.innerHeight / 2;
+                renderer.setSize( window.innerWidth, window.innerHeight );
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+            }
+function onDocumentMouseDown(){
+    event.preventDefault();
+    actionZ = -zoomSpeed;
 }
 
-function animate() {
-
-    requestAnimationFrame( animate );
-
-    render();
-
-}
-
-function render() {
-
-    var i = 0;
-
-    for ( var ix = 0; ix < AMOUNTX; ix ++ ) {
-
-        for ( var iy = 0; iy < AMOUNTY; iy ++ ) {
-
-            particle = particles[ i++ ];
-            particle.position.y = ( Math.sin( ( ix + count ) * 0.5 ) * 20 ) + ( Math.sin( ( iy + count ) * 0.5 ) * 20 );
-            particle.scale.x = particle.scale.y = ( Math.sin( ( ix + count ) * 0.3 ) + 2 ) * 4 + ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 4;
-
-        }
-
-    }
-
-    renderer.render( scene, camera );
-
-    // This increases or decreases speed
-    count += 0.2;
-
+function onDocumentMouseUp(){
+   actionZ = 0;
 }
